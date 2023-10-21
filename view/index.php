003af6e9e1d2a9c613e208f6d103c497e0161761
jortegaf <101069888+jortegaf@users.noreply.github.com>
2023-10-21 00:32:37 -0600

    se añadieron los archivos

--- view/index.php
+++ view/index.php
@@ -85,18 +85,18 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
       <img src="../plantilla/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">SISTEMA TRAMITE</span>
+      <span class="brand-text font-weight-light">API SIGED</span>
     </a>
 
     <!-- Sidebar -->
     <div class="sidebar">
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <img src="../plantilla/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+        <div class="image"> 
+          <img src="../plantilla/dist/img//user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="#" class="d-block">ANTTEC 2023</a>
         </div>
       </div>
 
